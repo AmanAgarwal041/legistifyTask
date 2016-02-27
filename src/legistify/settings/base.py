07a400983a8777bdb5bdbcff72479cff6cc583e2
AmanAgarwal041 <agarwal.aman041@gmail.com>
2016-02-27 23:57:09 +0530

    changed static url

--- src/legistify/settings/base.py
+++ src/legistify/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -99,8 +99,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT= "/home/aman/Desktop/legistify/static/"
+STATIC_ROOT= os.path.join(BASE_DIR,'/static/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 )
